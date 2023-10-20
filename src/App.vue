2251342d434567d01dc56929ead00dd8d44db575
Remenos <remenoss@gmail.com>
2023-10-20 07:09:47 +0300

    [RSC-17] Добавлен основной футер сайта.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="margin-bottom: 1000px;"></div>
+    <MainFooter></MainFooter>
+  </template>
+  
+  <script lang="ts">
+  import MainFooter from '../src/components/MainFooter.vue';
+  
+  export default {
+    components: {
+      MainFooter,
+    },
+  };
+  </script>
+  
+  <style>
+    body {
+      margin: 0px;
+    }
+  </style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer class="bg-green-300 text-white py-6">
+    <div class="container mx-auto flex items-start">
+      <div class="flex flex-col items-start">
+        <img src="../assets/logo lg.png" alt="Logo" class="h-16 w-auto" />
+
+        <p class="mt-1 font-bold text-lg text-black">Мы в соцсетях:</p>
+
+        <div class="flex">
+          <img
+            src="../assets/akar-icons_facebook-fill.png"
+            alt="Facebook"
+            class="h-8 w-8 mx-2"
+          />
+
+          <img
+            src="../assets/akar-icons_youtube-fill.png"
+            alt="YouTube"
+            class="h-8 w-8 mx-2"
+          />
+
+          <img
+            src="../assets/ant-design_instagram-filled.png"
+            alt="Instagram"
+            class="h-8 w-8 mx-2"
+          />
+        </div>
+      </div>
+
+      <div class="ml-32 flex-grow flex space-x-8">
+        <div class="flex-grow">
+          <p class="font-bold text-black">Что-то</p>
+          <ul class="list-none text-gray-800 pl-0">
+            <li class="mb-2">Текст</li>
+            <li class="mb-2">Текст</li>
+            <li class="mb-2">Текст</li>
+          </ul>
+        </div>
+
+        <div class="flex-grow">
+          <p class="font-bold text-black">Информация о нас</p>
+          <ul class="list-none text-gray-800 pl-0">
+            <li class="mb-2">Текст</li>
+            <li class="mb-2">Текст</li>
+            <li class="mb-2">Текст</li>
+          </ul>
+        </div>
+
+        <div class="flex-grow">
+          <p class="font-bold text-black">Контакты</p>
+          <ul class="list-none text-gray-800 pl-0">
+            <li class="mb-2">Номер:</li>
+            <li class="mb-2">8 999 888 22 55</li>
+          </ul>
+        </div>
+
+        <div class="flex-grow">
+          <p class="font-bold text-black">Адрес</p>
+          <ul class="list-none text-gray-800 pl-0">
+            <li class="mb-2">г. Волгоградская, ул.</li>
+            <li class="mb-2">Волгоградская 1</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
