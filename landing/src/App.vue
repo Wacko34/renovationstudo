a2adf27d51deff6dae47302fd4293eac3e3f559e
Nikita Sidorov <n.sidorov@intervolga.ru>
2023-10-21 21:25:34 +0300

    RSL-9: Создал скелет vue-приложения.

---
+++ landing/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/img/logo.png" />
+
+    <div class="wrapper">
+      <TheWelcome msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ landing/src/views/Landing.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import TheWelcome from '../components/TheWelcome.vue'
+  document.title = 'ПРИстройка';
+</script>
+
+<template>
+  <main>
+    body
+  </main>
+</template>
